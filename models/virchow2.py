e8da59478adceb9921641e6d711a98b4e600ae68
Amirb1900 <50403863+Amirb1900@users.noreply.github.com>
2026-07-31 13:02:27 +0330

    Create virchow2.py

---
+++ models/virchow2.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import timm
+
+from timm.layers import SwiGLUPacked
+
+
+class Virchow2Encoder(nn.Module):
+    """
+    Virchow2 Feature Extractor
+
+    Output:
+        cls_token    : (B, 1280)
+        reg_tokens   : (B, 4, 1280)
+        patch_tokens : (B, 256, 1280)
+    """
+
+    def __init__(self, freeze_backbone: bool = False):
+        super().__init__()
+
+        self.backbone = timm.create_model(
+            "hf-hub:paige-ai/Virchow2",
+            pretrained=True,
+            mlp_layer=SwiGLUPacked,
+            act_layer=torch.nn.SiLU,
+        )
+
+        if freeze_backbone:
+            print("Freezing Virchow2 backbone...")
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+
+        tokens = self.backbone(x)
+
+        cls_token = tokens[:, 0]
+        reg_tokens = tokens[:, 1:5]
+        patch_tokens = tokens[:, 5:]
+
+        return cls_token, reg_tokens, patch_tokens
